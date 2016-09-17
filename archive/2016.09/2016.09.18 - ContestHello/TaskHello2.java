27e54207a7f4b9fc0cac4affc1334da7d41f0ebc
meijun <jun.mei@outlook.com>
2016-09-18 02:13:02 +0800

    init

---
+++ archive/2016.09/2016.09.18 - ContestHello/TaskHello2.java
@@ -0,0 +1,20 @@
+package main;
+
+import io.github.meijun.misc.io.Scanner;
+import io.github.meijun.misc.io.Printer;
+import java.util.*;
+
+public class TaskHello2 {
+    Scanner in;
+    Printer out;
+
+    public void solve(int testNumber, Scanner in, Printer out) {
+        this.in = in;
+        this.out = out;
+        run();
+    }
+
+    void run() {
+        out.println(in.nextInt() + 1);
+    }
+}
