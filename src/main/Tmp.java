0b293ebf8b7fc6654ff0fea78d84b9759c1136b4
meijun <jun.mei@outlook.com>
2018-02-17 21:33:28 +0800

    include src/main

---
+++ src/main/Tmp.java
@@ -0,0 +1,13 @@
+package main;
+
+import lib.misc.$;
+
+public class Tmp {
+    void run() {
+        $.debug("Hello");
+    }
+
+    public static void main(String[] args) {
+        new Tmp().run();
+    }
+}
